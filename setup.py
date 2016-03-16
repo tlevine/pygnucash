60317d686186c8ad74cb74450b5edafad6844291
Thomas Levine <_@thomaslevine.com>
2016-03-16 22:55:27 +0000

    maintainer name

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from distutils.core import setup
 setup(name='pygnucash',
       author='Matthias Braun',
       author_email='matze@braunis.de',
+      maintainer='Thomas Levine',
+      maintainer_email='_@thomaslevine.com',
       description='read gnucash files in python',
       url='https://github.com/MatzeB/pygnucash',
       py_modules=['gnucash', 'gnucashutil'],
       scripts=['edit.py', 'gnucash2ledger.py', 'stockreport.py'],
-      version='1.0.0',
+      version='0.1',
       license='2-clause BSD',
 )
