0ed5baf1dc0fbcb95deda9ab5e9493107c8e2003
Thomas Levine <_@thomaslevine.com>
2016-03-16 22:52:33 +0000

    include scripts

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='pygnucash',
       description='read gnucash files in python',
       url='https://github.com/MatzeB/pygnucash',
       py_modules=['gnucash', 'gnucashutil'],
+      scripts=['edit.py', 'gnucash2ledger.py', 'stockreport.py'],
       version='1.0.0',
       license='2-clause BSD',
 )
