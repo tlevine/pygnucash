962920b4a2094e5d404691ecd537e4fb224c3d64
Thomas Levine <_@thomaslevine.com>
2016-03-16 22:47:59 +0000

    write setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pygnucash',
+      author='Matthias Braun',
+      author_email='matze@braunis.de',
+      description='read gnucash files in python',
+      url='https://github.com/MatzeB/pygnucash',
+      py_modules=['gnucash', 'gnucashutil'],
+      version='1.0.0',
+      license='2-clause BSD',
+)
